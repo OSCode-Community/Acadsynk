5a7fe25ae58a41123fb4cee33ab980acd28f9aca
Harsh kumar <105376726+mksharsh568@users.noreply.github.com>
2024-01-20 01:14:41 +0530

    google sheet

--- assets/js/main.js
+++ assets/js/main.js
@@ -212,3 +212,11 @@
   new PureCounter();
 
 })()
+
+document.querySelector("#show-login").addEventListener("click",function(){
+  document.querySelector(".popup").classList.add("active");
+});
+
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+  document.querySelector(".popup").classList.remove("active");
+});
